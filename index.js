3fd544c77bae429236d8e3d2ab8d5e361b84d591
bilalmuhammad14-7-98 <bilalmuhammad266@yahoo.com>
2021-12-26 18:59:23 +0500

    Working with Numbers and Strngs

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-const num = 30;
-console.log(num, "number");
+// NUMBERS
+
+const num = 10;
+console.log(num + num, "number");
+
+console.log(typeof num);
+
+console.log(Math.random());
+console.log(Math.PI);
+
+// to convert string into numbers or use + (plus sign)
+console.log(parseInt("20") + num);
+console.log(+"12");
+
+// it will give us an error Nan (not a number)
+console.log(parseInt("hello"));
+
+// strings (sequences of unicode characters);
+
+console.log("welcome to pakisatan");
+console.log("super".length);
+
+// to find index of string
+console.log("Bilal".charAt(0));
+
+// concate strings
+console.log("hello" + "  " + "welcome" + "Pakisatn");
+
+console.log("welcome to pakistan".toUpperCase());
