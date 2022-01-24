f8973e1cbf766786059379d318b9a15bf8c02f64
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2022-01-24 17:14:09 +0500

    Working with arrays

--- index.js
+++ index.js
@@ -48,37 +48,70 @@
 
 // Collections of key- value pairs
 
-var myName = { 1: "Bilal", 2: "usman", 3: "ahsan", 4: "ali" };
-console.log(myName);
+// var myName = { 1: "Bilal", 2: "usman", 3: "ahsan", 4: "ali" };
+// console.log(myName);
 
-var data = {
-  firstname: "Muhammad",
-  lastname: "Bilal",
-  age: 23,
-  phone: 555 - 555 - 555,
-  numbers: {
-    phone: 5555,
-    ptcl: 2120,
-    landline: 02 - 9123,
-  },
-  sayHi: function () {
-    console.log("Hello");
-  },
-};
+// var data = {
+//   firstname: "Muhammad",
+//   lastname: "Bilal",
+//   age: 23,
+//   phone: 555 - 555 - 555,
+//   numbers: {
+//     phone: 5555,
+//     ptcl: 2120,
+//     landline: 02 - 9123,
+//   },
+//   sayHi: function () {
+//     console.log("Hello");
+//   },
+// };
 
-console.log(data);
+// console.log(data);
 
-// Constructor pattern for creating objects
+// // Constructor pattern for creating objects
 
-function DounutConstructor(type, glazed, sweetness, hasChoclate) {
-  this.type = type;
-  this.glazed = glazed;
-  this.sweetness = sweetness;
-  this.hasChoclate = hasChoclate;
-}
+// function DounutConstructor(type, glazed, sweetness, hasChoclate) {
+//   this.type = type;
+//   this.glazed = glazed;
+//   this.sweetness = sweetness;
+//   this.hasChoclate = hasChoclate;
+// }
 
-var coconutDounut = new DounutConstructor("coconut", false, 10, true);
-var vanillaDounut = new DounutConstructor("vanilla", true, 20, false);
+// var coconutDounut = new DounutConstructor("coconut", false, 10, true);
+// var vanillaDounut = new DounutConstructor("vanilla", true, 20, false);
 
-console.log(coconutDounut);
-console.log(vanillaDounut);
+// console.log(coconutDounut);
+// console.log(vanillaDounut);
+
+// Arrays
+// Collection of data
+
+var myArr = [1, 2, 3, "Muhammad", "Bilal"];
+console.log(myArr, "ARRRRRRRRRR");
+console.log(myArr.length, "LENGTH");
+
+var nameArr = ["Bilal", "hamza", "sami"];
+nameArr.push("Waldo");
+console.log(nameArr, "arr");
+nameArr.pop();
+console.log(nameArr, "after pop");
+nameArr.push(["skbz", 100, 300, "bilal"]);
+console.log(nameArr[3]);
+console.log(nameArr[3]);
+
+var numArr = [3, 2, 1];
+var charArr = ["a", "d", "b", "c"];
+console.log(charArr.sort(), "sort");
+console.log(numArr.sort(), "sort");
+
+console.log(charArr.reverse(), "rev");
+
+console.log(numArr.concat(charArr, nameArr), "concat");
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var length = arr.length;
+console.log(arr[length - 2], "arrrrrrrrrrrrrrrrrrrrrrr");
+
+var slicearr = arr.slice(1, 8);
+console.log(slicearr, "sliced arr");
+console.log(arr, "after slice");
